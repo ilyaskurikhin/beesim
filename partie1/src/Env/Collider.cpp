9af29ece67c97a7ecea6c5c6d32716ae9ec20b08
Julia Besson <julia.besson@epfl.ch>
2016-03-14 17:45:20 +0100

    Initial commit

---
+++ partie1/src/Env/Collider.cpp
@@ -0,0 +1,11 @@
+#include <Collider.hpp>
+#include <iostream>
+#include <Application.hpp>
+
+using namespace std;
+
+Collider::Collider(Vec2d pos, double r) {
+	radius=r;
+	position=pos;
+}
+
---
+++ partie1/src/Env/Collider.hpp
@@ -0,0 +1,18 @@
+#ifndef COLLIDER_H
+#define COLLIDER_H
+#include <"Vec2d">
+
+class Collider {
+	
+private:
+Vec2d pos;
+double radius;
+
+
+public:
+Collider (Vec2d pos, double r);
+
+	
+};
+
+#endif
