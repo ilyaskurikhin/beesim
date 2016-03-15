7e93e886fe66499d2848b5cf3f9fa6b2af10a285
ilyaskurikhin <ilya@skurikhin.ch>
2016-03-15 09:28:36 -0400

    minor formatting changes

--- partie1/src/Env/Collider.cpp
+++ partie1/src/Env/Collider.cpp
@@ -4,74 +4,79 @@
 #include <Application.hpp>
 #include <cassert>
 
-
 using namespace std;
 
-	Collider::Collider(Vec2d position, double radius)
-		// affecte par défaut le rayon et la position
-		: radius_(radius), position_(position)
-		
-		
-		{ 	//si le rayon<0 envoie un message d'erreur "assertion failed"
-			assert (radius_ >= 0); 
+
+Collider::Collider(Vec2d position, double radius)
+	// affecte par défaut le rayon et la position
+	: radius_(radius), position_(position)
+{ 
+	//si le rayon<0 envoie un message d'erreur "assertion failed"
+	assert (radius_ >= 0); 
+	
+	//fait appelle a la méthode clamping pour corriger la position entrée
+	clamping();
+}
+
+
+	 
+Collider::Collider(const Collider& col) {
+	radius_=col.getRadius();
+	position_=col.getPosition();
+}
+
+
+	
+Collider& 
+operator= (const Collider& col) {
+	Collider tmp(col);
+	swap(radius_,tmp.getRadius());
+	swap(position_,tmp.getPosition());
+	return *this;
+}
+	 
+Vec2d 
+Collider::clamping() {
 			
-			//fait appelle a la méthode clamping pour corriger la position entrée
-			clamping();
+		//permet d'obtenir largeur et hautueur du monde
+		auto worldSize = getApp().getWorldSize();
+		auto width  = worldSize.x;
+		auto height = worldSize.y;
+		
+		//tant que position en x <0, on lui incrémente la largeur du monde
+		//tant que position > largeur du monde, on lui décremente la largeur du monde
+		
+		while (position_.x < 0) {
+			position_.x += width;
 		}
-		 
-	Vec2d Collider::clamping() {
-				
-			//permet d'obtenir largeur et hautueur du monde
-			auto worldSize = getApp().getWorldSize();
-			auto width  = worldSize.x;
-			auto height = worldSize.y;
-				
-			//tant que position en x <0, on lui incrémente la largeur du monde
-			//tant que position > largeur du monde, on lui décremente la largeur du monde
 			
-			while (position_.x < 0) {
-				position_.x += width;
-			}
+		while(position_.x > width) {
+					position_.x -= width;
+		} 
 			
-			while(position_.x > width) {
-						position_.x -= width;
-			} 
-				
-			//idem pour position en y
-			while (position_.y < 0) {
-				position_.y += width;
-			}
+		//idem pour position en y
+		while (position_.y < 0) {
+			position_.y += width;
+		}
 			
-			while(position_.y > width) {
-						position_.y -= width;
-			} 
+		while(position_.y > width) {
+			position_.y -= width;
+		} 
+		
 		//retourne le nouveau vec2d position
 		return position_;
-	}
+}
 
-	Vec2d Collider::getPosition() const {
-		return position_;
-	}
 
-	double Collider::getRadius() const {
-		return radius_;
-	}
 
-	Collider::Collider(const Collider& col) {
+Vec2d 
+Collider::getPosition() const {
+	return position_;
+}
 
-		radius_=col.getRadius();
-		position_=col.getPosition();
-	
-	}
-	
-	Collider& operator= (const Collider& col) {
-		Collider tmp(col);
-		swap(radius_,tmp.getRadius());
-		swap(position_,tmp.getPosition());
-		return *this;
-	}
-	
-		
-	
-	
-	
+
+
+double 
+Collider::getRadius() const {
+	return radius_;
+}
