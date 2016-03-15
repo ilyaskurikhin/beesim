158812c9c072c4bffcd4046a48db471c78dbb1e7
ilyaskurikhin <ilya@skurikhin.ch>
2016-03-15 09:54:25 -0400

    Added prototypes
    Added prototype definitions for class operators and Boolean functions.

--- partie1/src/Env/Collider.hpp
+++ partie1/src/Env/Collider.hpp
@@ -9,10 +9,22 @@ class Collider
 	Collider (Vec2d position, double radius);
 	
 	//constructeur de copie
-	Collider (const Collider& col);
+	Collider (const Collider& collider);
 	
 	//surcharge de l'opérateur de copie
-	Collider& operator= (const Collider& col)
+	Collider& operator= (const Collider& collider);
+	
+	// return true if other is in this
+	bool operator> (const Collider& other);
+	
+	// return true if this is colliding with other
+	bool operator| (const Collider& other);
+	
+	// return true if point p is within radius_ of this
+	bool operator> (const Vec2d& p);
+	
+	// print the contents of this to a stream
+	ostream& operator<< ();
 	
 	//methode de clamping corrigeant la position
 	Vec2d clamping(); 
@@ -23,7 +35,15 @@ class Collider
 	//permet d'obtenir le rayon
 	double getRadius() const;
 	
+	// returns true if this is within radius of other
+	bool isColliderInside (const Collider& other);
 	
+	// return true if either of this or other are within
+	// the others radius
+	bool isColliding (const Collider& other);
+	
+	// return true is distance in between p and this < radius_
+	bool isPointInside (Vec2d p);
 	private:
 
 	Vec2d position_;
