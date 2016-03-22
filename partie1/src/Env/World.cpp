06a56209fb1943cba6609644abf76391acd3d185
Julia Besson <julia.besson@epfl.ch>
2016-03-22 15:58:14 +0100

    Added methodes for cache generation (2.1)
    Added prototypes and defintions of methods as defined in the project part 2.1
    
    Still not compiling, errors with enum type and illegal characters.

--- partie1/src/Env/World.cpp
+++ partie1/src/Env/World.cpp
@@ -1,21 +1,26 @@
+#include "World.hpp"
+#include "../Utility/Vertex.hpp"
 #include <Application.hpp>
-#include <World.hpp>
 #include <SFML/Graphics.hpp> 
 #include <vector>
 
 
 using namespace std;
 
+
 void
 World::reloadConfig() 
 {
 	
-	j::Value simulationWorld;
-	simulationWorld= getAppConfig()["simulation"]["world"];
-	
-	nbCells_= simulationWorld["cells"].toDouble();
-	cellSize_= simulationWorld["size"].toDouble()) / nbCells_;
-	cells_ = vector<Kind>((simulationWorld["size"].toDouble() + nbCells_ * nbCells_),Rock);
+	// numberbCulumns : number of cells in a row
+	numberColumns_= getAppConfig()["simulation"]["world"]["cells"].toDouble();
+	// cellSize : size of a cell in 'pixels'
+	cellSize_= getAppConfig()["simulation"]["world"]["size"].toDouble() / numberColumns_;
+	// make a vector representing a square grid of Rock
+	size_t numberCells(numberColumns_ * numberColumns_);
+	for (size_t i(0); i < numberCells; ++i) {
+		cells_.push_back(Rock);
+	}
 }
 
 
@@ -23,8 +28,34 @@ World::reloadConfig()
 void
 World::reloadCacheStructure() 
 {
-
+	grassVertexes_ = generateVertexes(getAppConfig()["simulation"]["world"]["textures"], numberColumns_, cellSize_);
+	waterVertexes_ = grassVertexes_;
+	rockVertexes_ = grassVertexes_;
+	
+	renderingCache_.create(numberColumns_* cellSize_, numberColumns_ * cellSize_);
 }
+
+
+
+void
+World::updateCache() 
+{
+	renderingCache_.clear();
+	sf::RenderStates rs;
+	rs.texture = &getAppTexture(getAppConfig()["simulation"]["world"]["textures"]["rock"].toString()); // ici pour la texture liée à la roche
+	renderingCache_.draw(rockVertexes_.data(), rockVertexes_.size(), sf::Quads, rs);
+	renderingCache_.display();
+}
+
+
+
+void
+World::reset()
+{
+	reloadConfig();
+	reloadCacheStructure();
+	updateCache();
+}	
 
 
 
@@ -34,4 +65,16 @@ World::drawOn(sf::RenderTarget& target)
 	sf::Sprite cache(renderingCache_.getTexture());
 	target.draw(cache);
 }
+	
+	
 	 
+	 	/*
+	// we'll need this someday
+	size_t numberVertexColumns(4 * numberColumns_);
+	size_t numberVertexRows(4 * numberColumns_);
+	for (size_t i(0); i < numberVertexRows; ++i) {
+		for (size_t j(0); j < numberVertexColumns; ++j) {
+			grassVertexes_[i * numberVertexColumns + j] = 
+		}
+	}
+	*/
--- partie1/src/Env/World.hpp
+++ partie1/src/Env/World.hpp
@@ -14,25 +14,33 @@ class World
 {
     public:
         
-        // 
-        void
-	reloadConfig();
         
-
-
-	void
-        reloadCacheStructure();
-
-
+		void
+		reloadConfig();
+        
+		void
+		reloadCacheStructure();
 
         void
         drawOn(sf::RenderTarget& target);
 
+		void 
+		updateCache();
+		
+		void 
+		reset();
+		
 
     private:
-        int nbColumns_;
+        int numberColumns_;
         float cellSize_;
-        vector<Kind> cells_;
+        std::vector<Kind> cells_;
+        
         sf::RenderTexture renderingCache_;
+        std::vector<sf::Vertex> grassVertexes_;
+        std::vector<sf::Vertex> waterVertexes_;
+        std::vector<sf::Vertex> rockVertexes_;
+        
+        bool regenerate_;
 };
 #endif
