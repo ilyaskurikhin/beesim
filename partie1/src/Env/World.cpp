5c25cbb21c4d94fc28f372fba3a41b10d57eb737
Ilya Skurikhin <ilya@skurikhin.ch>
2016-04-04 15:17:19 +0200

    more buggs

--- partie1/src/Env/World.cpp
+++ partie1/src/Env/World.cpp
@@ -65,34 +65,30 @@ World::reloadCacheStructure()
 void
 World::updateCache() 
 {
-
 	for (size_t i(0); i < numberColumns_; ++i) {
 		for (size_t j(0); j < numberColumns_; ++j) {
 			
-			vector <long unsigned int> Indexes = indexesForCellVertexes(i, j, numberColumns_);
+			vector <size_t> indexes = indexesForCellVertexes(i, j, cellSize_);
 			
-			if (cells_[j + i * numberColumns_] == Kind::Rock) {
-				for (size_t i(0); i<=Indexes.size(); ++i) {
-						cout <<"Hello2";
-
-				waterVertexes_[Indexes[i]].color.a=0;
-				grassVertexes_[Indexes[i]].color.a=0;
-				rockVertexes_[Indexes[i]].color.a=255;
-
+			if (cells_[(i * numberColumns_) + j] == Kind::Rock) {
+				for (size_t k(0); k<=indexes.size(); ++k) {
+				waterVertexes_[indexes[k]].color.a=0;
+				grassVertexes_[indexes[k]].color.a=0;
+				rockVertexes_[indexes[k]].color.a=255;
 				}
 			}
-			else if (cells_[j + i * numberColumns_] == Kind::Grass) {
-				for (size_t i(0); i<=Indexes.size(); ++i) {
-				waterVertexes_[Indexes[i]].color.a=0;
-				grassVertexes_[Indexes[i]].color.a=255;
-				rockVertexes_[Indexes[i]].color.a=0;
+			else if (cells_[(i * numberColumns_) + j] == Kind::Grass) {
+				for (size_t k(0); k<=indexes.size(); ++k) {
+				waterVertexes_[indexes[k]].color.a=0;
+				grassVertexes_[indexes[k]].color.a=255;
+				rockVertexes_[indexes[k]].color.a=0;
 				}
 			} 
-			else if (cells_[j + i * numberColumns_] == Kind::Water) {
-				for (size_t i(0); i<=Indexes.size(); ++i) {
-				waterVertexes_[Indexes[i]].color.a=255;
-				grassVertexes_[Indexes[i]].color.a=0;
-				rockVertexes_[Indexes[i]].color.a=0;
+			else if (cells_[(i * numberColumns_) + j] == Kind::Water) {
+				for (size_t k(0); k<=indexes.size(); ++k) {
+				waterVertexes_[indexes[k]].color.a=255;
+				grassVertexes_[indexes[k]].color.a=0;
+				rockVertexes_[indexes[k]].color.a=0;
 				}
 			}
 			
@@ -125,6 +121,9 @@ World::reset(bool regenerate)
 	reloadConfig();
 	reloadCacheStructure();
 	updateCache();
+        if (regenerate) {
+            // do things here
+        }
 }	
 
 
@@ -146,39 +145,34 @@ World::loadFromFile()
 	ifstream entree(fileName.c_str());
 	ofstream sortie;
 		
-	try
-	{
-	if (entree.fail()) throw std::runtime_error ("Pas de fichier correspondant");
+	try {
+            if (entree.fail()) throw std::runtime_error ("Pas de fichier correspondant");
 
-	string numberColumns;
-	string cellSize;
-	short var;
-	
-	entree >> numberColumns;
-	//le nombre de colonnes vaut cette valeur convertie en entier
-	numberColumns_ = stoi(numberColumns);
-	entree >> std::ws;
+            string numberColumns;
+            string cellSize;
+            short var;
+            
+            entree >> numberColumns;
+            //le nombre de colonnes vaut cette valeur convertie en entier
+            numberColumns_ = stoi(numberColumns);
+            entree >> std::ws;
 
-	entree >> cellSize;
-	//la taille de la cellule vaut cette valeur convertie en Float
-	cellSize_= stof(cellSize);
-	entree >> std::ws;
-	
-	for (size_t i(0); i <= numberColumns_; ++i) {
-	// var est de type short
-	entree >> var;
-	
-	//il faut convertir var en  Kind
-	Kind values = static_cast<Kind>(var);
-	cells_.push_back(values);
-	entree >> std::ws;
-	}
-	}
-	
-	catch (std::runtime_error error) 
-	{
-
-	std::cerr << error;
+            entree >> cellSize;
+            //la taille de la cellule vaut cette valeur convertie en Float
+            cellSize_= stof(cellSize);
+            entree >> std::ws;
+            
+            for (size_t i(0); i <= numberColumns_; ++i) {
+                // var est de type short
+                entree >> var;
+                
+                //il faut convertir var en  Kind
+                Kind value = static_cast<Kind>(var);
+                cells_.push_back(value);
+                entree >> std::ws;
+	    }
+	} catch (std::runtime_error error) {
+            std::cerr << error;
 	}
 	
 	entree.close();	
